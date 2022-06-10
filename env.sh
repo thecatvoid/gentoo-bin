4b4e0a9e04af205c24e1f975091813a2bc65ecf8
void <92902636+thecatvoid@users.noreply.github.com>
2022-06-10 07:20:57 +0530

    work?

--- env.sh
+++ env.sh
@@ -4,11 +4,9 @@
 set -ex
 
 # Downloads Stage 3 Gentoo Targall and unpacks it (SYSTEMD)
-mkdir -p /gentoo
-
 curl -Lsq "https://github.com/thecatvoid/gentoo-wine-tar/releases/download/latest/gentoo.tar.gz" -o gentoo.tar.gz
-tar xpf gentoo.tar.gz --xattrs-include='*.*' --numeric-owner -C /gentoo
-rm -rf ./*.xz /gentoo/etc/portage
+tar xpf gentoo.tar.gz --xattrs-include='*.*' --numeric-owner -C /
+rm -rf ./gentoo.tar.gz /gentoo/etc/portage
 
 # Misc
 mkdir -p /gentoo/etc/portage
@@ -20,4 +18,4 @@ mv /gentoo/etc/portage/repos.conf/gentoo.conf /gentoo/root/
 
 # Chroot env
 cp -L /etc/resolv.conf /gentoo/etc/resolv.conf
-for i in dev sys run tmp proc; do mount -R /$i /gentoo/$i; done
+for i in dev sys run tmp proc; do mkdir -p /gentoo/$i && mount -R /$i /gentoo/$i; done
